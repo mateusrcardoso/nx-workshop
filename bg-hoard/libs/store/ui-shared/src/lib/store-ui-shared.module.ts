59d293fbe387821a21f30fe233c2d1f655a932a5
mateusrcardoso <mateusrcardoso@gmail.com>
2021-04-13 07:52:47 -0300

    New component created inside 'libs' folder.

--- bg-hoard/libs/store/ui-shared/src/lib/store-ui-shared.module.ts
+++ bg-hoard/libs/store/ui-shared/src/lib/store-ui-shared.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { UiSharedComponent } from './ui-shared/ui-shared.component';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [
+    UiSharedComponent
+  ],
 })
 export class StoreUiSharedModule {}
---
+++ bg-hoard/libs/store/ui-shared/src/lib/ui-shared/ui-shared.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'bg-hoard-ui-shared',
+  templateUrl: './ui-shared.component.html',
+  styleUrls: ['./ui-shared.component.css']
+})
+export class UiSharedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
